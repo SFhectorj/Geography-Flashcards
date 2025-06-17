7845c7067d97b84290f3016e10e12735144266fc
Hector Baeza <baezahectorj@gmail.com>
2025-06-17 00:08:49 -0700

    Created data directory & flashcards.js: Includes data to be displayed by the flashcards.

---
+++ src/data/flashcards.js
@@ -0,0 +1,52 @@
+const flashcards = [
+  {
+    question: "What is the capital of France?",
+    answer: "Paris",
+    image: "/images/france.png",
+    category: "Easy"
+  },
+  {
+    question: "Which river is the longest in the world?",
+    answer: "The Nile",
+    image: "/images/nile.png",
+    category: "Medium"
+  },
+  {
+    question: "Which country has the most time zones?",
+    answer: "France",
+    image: "/images/timezones.png",
+    category: "Hard"
+  },
+  {
+    question: "Which desert is the largest in the world?",
+    answer: "The Antarctic Desert",
+    image: "/images/antarctica.png",
+    category: "Hard"
+  },
+  {
+    question: "What is the smallest country in the world?",
+    answer: "Vatican City",
+    image: "/images/vatican.png",
+    category: "Easy"
+  },
+  {
+    question: "Mount Everest is part of which mountain range?",
+    answer: "The Himalayas",
+    image: "/images/everest.png",
+    category: "Medium"
+  },
+  {
+    question: "Which U.S. state has the most active volcanoes?",
+    answer: "Alaska",
+    image: "/images/alaska.png",
+    category: "Medium"
+  },
+  {
+    question: "Which African lake is the world's second largest freshwater lake by surface area?",
+    answer: "Lake Victoria",
+    image: "/images/victoria.png",
+    category: "Hard"
+  }
+];
+
+export default flashcards;
